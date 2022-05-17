6566f380400a322dc5c835d8abb92a129ff8a0fe
Cameron159753 <cameronbowers22@gmail.com>
2022-05-17 11:19:06 +0200

    todo list

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app id="inspire">
+
+    <v-app-bar
+      app
+      color="#primary"
+      dark
+      prominent
+      src="https://i.postimg.cc/tRtvvqfx/To-Do-List-Logo-for-Facebook.webp"
+    >
+     
+      <!-- <v-app-bar-title> Vuetify Todo</v-app-bar-title> -->
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      items: [
+          { title: 'Todo', icon: 'mdi-format-list-checks', to:'/' },
+          { title: 'About', icon: 'mdi-help-box', to:'/about' },
+        ],
+    }),
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Todo.vue
@@ -0,0 +1,94 @@
+<template>
+<div class="home">
+            <v-text-field
+            v-model="newTaskTitle"
+            @click:append="addTask"
+            @keyup.enter="addTask"
+            class="pa-3"
+            outlined
+            label="Add task"
+            append-icon="mdi-plus"
+            hide-details
+            clearable
+          ></v-text-field>
+    <v-list
+    class="pt-0"
+      flat
+    >
+    <div
+        v-for="task in tasks"
+        :key="task.id"
+        >
+        <v-list-item 
+        @click="doneTask(task.id)"
+        :class="{ 'brown lighten-2' : task.done}"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+              :class="{ 'text-decoration-line-through' : task.done }">
+                {{ task.title }}
+                </v-list-item-title>
+            </v-list-item-content>
+
+                  <v-list-item-action>
+          <v-btn 
+          @click.stop="deleteTask(task.id)"
+          icon>
+            <v-icon color="grey lighten-1">mdi-delete</v-icon>
+          </v-btn>
+        </v-list-item-action>
+          </template>
+
+        </v-list-item>
+        <v-divider></v-divider>
+    </div>
+
+    </v-list>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+    data() {
+      return {
+        STORAGE_KEY: 'vue-todo-app-storage',
+        newTaskTitle: "",
+        tasks: [],
+        newTask: ""
+      }
+    },
+    created() {
+      this.tasks = JSON.parse(localStorage.getItem(this.STORAGE_KEY) || '[]')
+      console.log(this.tasks)
+    },
+    methods: {
+
+      addTask() {
+        let newTask = {
+          id: Date.now(),
+          title: this.newTaskTitle,
+          done: false
+        }
+        this.tasks.push(newTask)
+        this.newTaskTitle = ''  
+        localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.tasks));
+      },
+      doneTask(id) {
+        let task = this.tasks.filter( task => task.id === id) [0]
+        task.done = !task.done
+        localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.tasks));
+      },
+      deleteTask(id) {
+        this.tasks = this.tasks.filter(task => task.id !== id)
+        localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.tasks));
+      }
+    }
+  }
+  
+</script>
